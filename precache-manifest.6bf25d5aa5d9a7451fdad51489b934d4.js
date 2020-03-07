7f866641e2b849f40b9db4a1452794b5b1931d2e
Mukul Debnath <debnath.mukul@gmail.com>
2020-03-07 20:45:13 +0100

    Updates

---
+++ precache-manifest.6bf25d5aa5d9a7451fdad51489b934d4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "902612345ead8141e4e728ca472add16",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "8f9d1b9d0ea8f31bad2f",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "38ffd2dd036d69e7b2d5",
+    "url": "/robofriends/static/css/main.0c4b3273.chunk.css"
+  },
+  {
+    "revision": "8f9d1b9d0ea8f31bad2f",
+    "url": "/robofriends/static/js/2.5b396b40.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/robofriends/static/js/2.5b396b40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38ffd2dd036d69e7b2d5",
+    "url": "/robofriends/static/js/main.9eaf8559.chunk.js"
+  },
+  {
+    "revision": "5677d8ff5cb4c373cfaa",
+    "url": "/robofriends/static/js/runtime-main.c712519d.js"
+  }
+]);
